175d15e91c7a83f2ad18307330bbe1ff4128cf36
Shao <shaotang.yen@gmail.com>
2021-11-06 11:22:40 +1100

    file structure setup, added routes, helper codes, middleware code, db id update, initial notes.js code

---
+++ Develop/routes/notes.js
@@ -0,0 +1,61 @@
+const noted = require("express").Router();
+const path = require('path');
+const uuid = require("../helpers/uuid");
+const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtilities");
+
+// GET Route to retrive notes
+noted.get("/", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+noted.get("/:id", (req, res) => {
+    const noteId = req.params.id;
+    readFromFile("./db/db.json")
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            console.log(json);
+            const result = json.filter((note) => note.id === noteId);
+            return result.length > 0
+                ? res.json(result)
+                : res.json("No note with that ID");
+        });
+});
+
+// POST Route for a new note
+noted.post("/", (req, res) => {
+
+    console.log(req.body);
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            id: uuid(),
+            title,
+            text,
+        };
+
+        readAndAppend(newNote, "./db/db.json");
+        res.json(`Note added.`);
+    } else {
+        res.error("There was a problem adding a note.");
+    }
+});
+
+// DELETE Route for a specific note
+noted.delete("/:id", (req, res) => {
+    const noteId = req.params.id;
+    readFromFile("./db/db.json")
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            // Make a new array of all notes except the one with the ID provided in the URL
+            console.log(json);
+            const result = json.filter((note) => note.id !== noteId);
+            // Save that array to the filesystem
+            writeToFile("./db/db.json", result);
+            // Respond to the DELETE request
+            res.json(`Your note with id: ${noteId} has been deleted`);
+
+        });
+});
+
+module.exports = noted;
