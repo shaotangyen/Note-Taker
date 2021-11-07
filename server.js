605bba524ee5218a4c294e9b9103f1c56c6585d5
Shao <shaotang.yen@gmail.com>
2021-11-07 23:43:24 +1100

    updating PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Import custom middleware
@@ -30,6 +30,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(port, () =>
-  console.log(`App listening at http://localhost:${PORT}.`)
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}}.`)
 );
