3b28ad85b6cbff530497dea6ba1773847f3a0ab2
Shao <shaotang.yen@gmail.com>
2021-11-07 23:37:53 +1100

    bug fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
-const PORT = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 const app = express();
 
 // Import custom middleware
@@ -30,6 +30,6 @@ app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.listen(PORT, () =>
+app.listen(port, () =>
   console.log(`App listening at http://localhost:${PORT}.`)
 );
